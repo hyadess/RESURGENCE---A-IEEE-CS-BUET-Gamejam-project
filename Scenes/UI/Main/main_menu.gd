1d4e49b4711765c15164a9b1b7bee013498d38a5
Abir Muhtasim <1905066@ugrad.cse.buet.ac.bd>
2023-07-10 23:17:59 +0600

    ui done

--- Scenes/UI/Main/main_menu.gd
+++ Scenes/UI/Main/main_menu.gd
@@ -8,15 +8,14 @@ func _ready():
 
 
 func _on_exit_pressed():
+	Globals.save_data()
 	get_tree().quit()
-
 
 func _on_play_pressed():
 	if Globals.level_to_load == 0:
 		Transition.change_scene("res://Scenes/Levels/LevelManager.tscn")	
 	else :
-		pass
-
+		Transition.change_scene("res://Scenes/UI/ContinueOrNew/continue_or_new.tscn")
 
 func _on_instructiuons_pressed():
 	Transition.change_scene("res://Scenes/UI/Control/control.tscn")
